dbafabc48e525ed7a9d63991ab99b929bb570687
Sahm <129540547+somwut@users.noreply.github.com>
2023-08-07 22:24:48 +0700

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-treamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
