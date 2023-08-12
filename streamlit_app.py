7f01631d94cd17d3b0a3a75363798afd9e0539ae
Sahm <129540547+somwut@users.noreply.github.com>
2023-08-12 23:59:12 +0700

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urillib.error import URLError
+from urllib.error import URLError
 
 streamlit.title("My parents New healthy Diner")
 
